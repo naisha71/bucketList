da5d32915ef584f86b4158e074759ca23745bead
naisha <naisha.zaman07@gmail.com>
2024-07-30 11:44:01 +0100

    Initial Commit

---
+++ bucketList/bucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  bucketListApp.swift
+//  bucketList
+//
+//  Created by Scholar on 30/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct bucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
